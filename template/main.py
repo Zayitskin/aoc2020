9cbce953adf33cdaa94a81d35ad6633dccb76c8d
Zachary Itskin <36995254+Zayitskin@users.noreply.github.com>
2020-12-08 23:23:11 -0600

    Add improved template

--- template/main.py
+++ template/main.py
@@ -1,6 +1,6 @@
 
 def load(path: str) -> list:
-
+    """Load the input from a file."""
     data: list = []
     with open(path) as f:
         return [line.strip() for line in f]
@@ -15,5 +15,7 @@ def part2(data: list) -> int:
 
 if __name__ == "__main__":
 
-    data = load(".txt")
+    #Load the input data
+    data: list = load(".txt")
+    #Print the answers for part 1 and 2
     print(f"Part 1: {part1(data)}\nPart 2: {part2(data)}")
